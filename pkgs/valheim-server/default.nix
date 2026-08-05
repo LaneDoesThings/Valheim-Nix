18b96db6c0eed2e0f0b2ec5e44dff2695a2b1223
Lane Wright <lane.wright2003@gmail.com>
2026-08-05 18:14:13 -0500

    valheim-server pkgs

---
+++ pkgs/valheim-server/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  stdenv,
+  fetchSteam,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  name = "valheim-server";
+  version = "0.219.13";
+  src = fetchSteam {
+    inherit (finalAttrs) name;
+    appId = "896660";
+    depotId = "896661";
+    manifestId = "7872048626245078252";
+    hash = "sha256-d1UbkdrTzu/TOjzND9F1iG9Jz8a850wtED+Id52qQcI=";
+  };
+
+  # Skip phases that don't apply to prebuilt binaries.
+  dontBuild = true;
+  dontConfigure = true;
+  dontFixup = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out
+    cp -r \
+      *.so \
+      *.debug \
+      valheim_server.x86_64 \
+      valheim_server_Data \
+      $out
+
+    chmod +x $out/valheim_server.x86_64
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Valheim dedicated server";
+    homepage = "https://steamdb.info/app/896660/";
+    changelog = "https://store.steampowered.com/news/app/892970?updates=true";
+    sourceProvenance = with sourceTypes; [
+      binaryBytecode
+      binaryNativeCode
+    ];
+    license = licenses.unfree;
+    maintainers = with maintainers; [ aidalgol ];
+    platforms = [ "x86_64-linux" ];
+  };
+})
